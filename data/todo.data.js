3e6188d6a964bdbc981fe9b13f886e9a1327cc5e
leee1846 <leee1846@gmail.com>
2020-12-09 20:36:13 +0900

    feature:  HTML구성

---
+++ data/todo.data.js
@@ -0,0 +1,27 @@
+let TODOS = [
+  {
+    id: 1,
+    category: "프로그래밍",
+    contents: "javascript",
+  },
+  {
+    id: 2,
+    category: "프로그래밍",
+    contents: "css",
+  },
+  {
+    id: 3,
+    category: "프로그래밍",
+    contents: "HTML",
+  },
+  {
+    id: 4,
+    category: "취미",
+    contents: "헬스",
+  },
+  {
+    id: 5,
+    category: "취미",
+    contents: "수영",
+  },
+];
---
+++ js/app.js
@@ -0,0 +1,3 @@
+const d = new Date();
+const a = d.getMonth() + 1;
+console.log(a - 1);
