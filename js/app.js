8232bde615fedd01397c97311d5c0ea108b33a80
leee1846 <leee1846@gmail.com>
2020-12-13 16:38:09 +0900

    refactoring : initApp 생성

--- js/app.js
+++ js/app.js
@@ -4,14 +4,18 @@ const dateRightArrow = document.querySelector("#right-date");
 const dateLeftArrow = document.querySelector("#left-date");
 const inputAddButton = document.querySelector("#todo-add-container>i");
 
-//처음 로드시 나올 화면 구성
-firstDefaultActions();
+const initApp = () => {
+  //처음 로드시 나올 화면 구성
+  firstDefaultActions();
 
-//---- 최상단 카테고리 클릭 이벤트
-todoCategoryElements.forEach(function (todoCategoryElement) {
-  todoCategoryElement.addEventListener("click", resetTodoListByCategory);
-});
-//---- 인풋내용 추가
-inputAddButton.addEventListener("click", addNewList);
-//----클릭 additional-pocket appear/disappear이벤트
-todoListContainer.addEventListener("click", editTodoList);
+  // 최상단 카테고리 클릭 이벤트
+  todoCategoryElements.forEach(function (todoCategoryElement) {
+    todoCategoryElement.addEventListener("click", resetTodoListByCategory);
+  });
+  // 인풋내용 추가
+  inputAddButton.addEventListener("click", addNewList);
+  //클릭 additional-pocket appear/disappear이벤트
+  todoListContainer.addEventListener("click", editTodoList);
+};
+
+initApp();
