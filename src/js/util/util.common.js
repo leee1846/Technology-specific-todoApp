d0cd0fbf4760fda978107da8b0e1177dd418d7f6
leee1846 <leee1846@gmail.com>
2021-01-12 12:14:58 +0900

    feature: 리액트 설치

--- src/js/util/util.common.js
+++
@@ -1,7 +0,0 @@
-//날짜 선택 구성
-const newDate = new Date();
-const thisYear = newDate.getFullYear();
-const thisMonth = newDate.getMonth() + 1;
-const thisDate = newDate.getDate();
-
-export { thisYear, thisMonth, thisDate };
