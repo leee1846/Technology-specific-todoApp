c02dbedf3623a52c10447b550b396b4dcd0f728c
leee1846 <leee1846@gmail.com>
2021-01-29 22:39:26 +0900

    feature: category filter한 리스트 리스트 컴포넌트로 가져오기

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoItem from "./../TodoItem/TodoItem";
 import * as Styled from "./TodoList.style";
+import { useSelector } from "react-redux";
 
 const TodoList = ({ todoReducer }) => {
+  const listByCategory = useSelector((state) => state.filterListReducer);
+
   return (
     <Styled.ListContainer>
       {todoReducer.map((list) => (
