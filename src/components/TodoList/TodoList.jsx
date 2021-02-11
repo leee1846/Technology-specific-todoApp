a8c89fc7c06778c1be9b29a7b371200ab4c544b1
leee1846 <leee1846@gmail.com>
2021-02-11 20:37:00 +0900

    feature: morebox off기능 주석 작성

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,6 +5,7 @@ import * as Styled from "./TodoList.style";
 const TodoList = ({ todoReducer }) => {
   const [currentIndex, setCurrentIndex] = useState(-1);
 
+  //리스트 내용 변경시 morebox off
   useEffect(() => {
     setCurrentIndex(-1);
   }, [todoReducer]);
