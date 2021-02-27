1e084ef296d1cd54b4a7edd8a7740cd98d683849
leee1846 <leee1846@gmail.com>
2021-02-27 17:40:25 +0900

    style: 기본 스타일 적용

---
+++ src/components/KakaoLogin/KakaoLogin.style.js
@@ -0,0 +1,10 @@
+import Styled from "styled-components";
+
+export const LoginImage = Styled.img`
+  transition:.1s;
+  cursor:pointer;
+
+  &:hover {
+    box-shadow:2px 2px 5px dodgerblue;
+  }
+`;
---
+++ src/pages/LoginPage/LoginPage.style.js
@@ -0,0 +1,8 @@
+import Styled from "styled-components";
+
+export const LoginContainer = Styled.div`
+  margin-top:100px;
+  padding:50px 10px;
+  background-color:rgba(255, 255, 255, 0.2);
+  border-radius:10px;
+`;
