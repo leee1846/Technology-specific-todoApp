383f292cde07d44f400cf6776b30568efa17d942
leee1846 <leee1846@gmail.com>
2021-03-06 16:53:56 +0900

    style: kakaologin 중앙배치

--- src/components/KakaoLogin/KakaoLogin.style.js
+++ src/components/KakaoLogin/KakaoLogin.style.js
@@ -1,10 +1,13 @@
 import Styled from "styled-components";
 
 export const LoginImage = Styled.img`
+  position:absolute;
+  top:50%;
+  transform:translateY(-50%);
   transition:.1s;
   cursor:pointer;
 
   &:hover {
-    box-shadow:2px 2px 5px dodgerblue;
+    box-shadow:5px 5px 5px rgba(9, 119, 230, 0.801);
   }
 `;
